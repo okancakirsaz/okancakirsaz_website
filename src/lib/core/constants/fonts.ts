634da462724574b17880af28b98be022cb584217
okancakirsaz <ocakirsaz@gmail.com>
2025-01-16 11:07:03 +0300

    some ui updates

---
+++ src/lib/core/constants/fonts.ts
@@ -0,0 +1,5 @@
+export default class Fonts{
+    static instance:Fonts = new Fonts();
+    passionOne = "Passion One";
+    montserrat = "Montserrat";
+}
--- src/lib/pages/main_page/main_page_view.tsx
+++ src/lib/pages/main_page/main_page_view.tsx
@@ -1,10 +1,12 @@
+import CustomSpecialButton from "../../core/components/custom_special_button";
+import Fonts from "../../core/constants/fonts";
 
 export default function MainPageView(){
 const welcomeSheetDesktop:React.CSSProperties = {
 backgroundColor: "#282729",//Dark grey
-fontFamily:"Passion One",
+fontFamily: Fonts.instance.passionOne,
 color:"white",
-width: "40%",
+width: "47%",
 height:"220px",
 borderRadius:"30px",
 margin:"40px"
@@ -19,9 +21,17 @@ return (
         marginLeft:"220px",
         marginTop:"30px",
         fontSize:"30px",
-    }} 
+    }}
     >Welcome to my<br></br>website!
     </span>
+    <div style={{height:"140px"}}></div>
+    <div style={{display:"flex",justifyContent:"space-evenly"}} >
+       <CustomSpecialButton text="Model Generator" onClick={()=>console.log("Model Generator")}></CustomSpecialButton>
+       <CustomSpecialButton text="My Works" onClick={()=>console.log("My Works")}></CustomSpecialButton>
+       <CustomSpecialButton text="Current Works" onClick={()=>console.log("Current Works")}></CustomSpecialButton>
+       <CustomSpecialButton iconUrl="/icons/instagram.svg" text="instagram" onClick={()=>console.log("instagram")}></CustomSpecialButton>
+       <CustomSpecialButton iconUrl="/icons/github.svg" text="instagram" onClick={()=>console.log("github")}></CustomSpecialButton>
+    </div>
 </div>
 </>
 );
